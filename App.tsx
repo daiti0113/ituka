59226c6006cb7168406e9ef702d97cdd01463430
daiti0113 <stennis.ds.0113@gmail.com>
2022-08-31 10:57:32 +0900

    feat: react-navigationを導入

--- App.tsx
+++ App.tsx
@@ -1,33 +1,53 @@
 import React from "react"
-import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    useColorScheme,
-} from "react-native"
-import { Colors } from "react-native/Libraries/NewAppScreen"
-import { Provider as PaperProvider, TextInput } from "react-native-paper"
+import { View } from "react-native"
+import { Button, Provider as PaperProvider, Text, TextInput } from "react-native-paper"
+import { NavigationContainer, useNavigation } from "@react-navigation/native"
+import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack"
 
+
+type RootStackParamList = {
+    Home: undefined
+    Details: undefined
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>
+
+const HomeScreen = () => {
+    const navigation = useNavigation<HomeScreenNavigationProp>()
+    return (
+        <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+            <Text>Home Screen</Text>
+            <TextInput label="Home" />
+            <Button onPress={() => navigation.navigate("Details")}>Go to Detail</Button>
+        </View>
+    )
+}
+
+type DetailsScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>
+
+const DetailsScreen = () => {
+    const navigation = useNavigation<DetailsScreenNavigationProp>()
+    return (
+        <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+            <Text>Details Screen</Text>
+            <TextInput label="Detail" />
+            <Button onPress={() => navigation.navigate("Home")}>Go to Home</Button>
+        </View>
+    )
+}
 
 const App = () => {
-    const isDarkMode = useColorScheme() === "dark"
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    }
-
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={backgroundStyle}
-            >
-                <PaperProvider>
-                    <TextInput label="テスト" />
-                </PaperProvider>
-            </ScrollView>
-        </SafeAreaView>
+        <NavigationContainer>
+            <PaperProvider>
+                <Stack.Navigator initialRouteName="Home">
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Details" component={DetailsScreen} />
+                </Stack.Navigator>
+            </PaperProvider>
+        </NavigationContainer>
     )
 }
 
