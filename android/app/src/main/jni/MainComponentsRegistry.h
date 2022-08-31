da096640764696adde66559b96a9e3a1c0fdbed7
daiti0113 <stennis.ds.0113@gmail.com>
2022-08-31 23:54:49 +0900

    chore: アプリの名前をicoccaに変更

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/ituka/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/icocca/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
