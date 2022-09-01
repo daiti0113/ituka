9803d2abab323f8c0d29d666516128a46b791edc
daiti0113 <stennis.ds.0113@gmail.com>
2022-09-01 23:07:46 +0900

    feat: TopNavを追加

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,16 +6,37 @@ import { useNavigation } from "@react-navigation/native"
 import { View } from "react-native"
 import { Button, Text, TextInput } from "react-native-paper"
 import { RootStackParamList } from "../../App"
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
 
-type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>
+const Tab = createMaterialTopTabNavigator()
+
+const data = [
+    {name: "Hitori", label: "一人で"},
+    {name: "Koibito", label: "恋人と"},
+    {name: "Tomodati", label: "友達と"},
+]
+
+const createTabs = (data) => data.map(({name, label}) => <Tab.Screen name={name} key={name} options={{tabBarLabel: label}} component={ToDoListScene} />)
 
 export const HomeScreen = () => {
+    const tabs = createTabs(data)
+    return (
+        <Tab.Navigator
+            initialRouteName="ToDo"
+            screenOptions={{tabBarIndicator: () => null}}
+        >
+            {tabs}
+        </Tab.Navigator>
+    )
+}
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>
+export const ToDoListScene = () => {
     const navigation = useNavigation<HomeScreenNavigationProp>()
     const {loggedIn} = useAppSelector(({auth: {loggedIn}}) => ({loggedIn}))
     const dispatch = useAppDispatch()
     return (
         <View style={{ padding: 10 }}>
-            <Text variant="headlineLarge">icocca</Text>
             <TextInput label="Home" />
             {loggedIn ? <Text>ログインしました</Text> : <Button onPress={() => dispatch(login())}>Login</Button>}
             <Button onPress={() => navigation.navigate("Details")}>Go to Detail</Button>
