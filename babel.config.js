aa4409c6dd19429ad1beec86220cc0c190528603
daiti0113 <stennis.ds.0113@gmail.com>
2022-09-14 01:40:20 +0900

    feat: firebase sdk の設定を追加

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,16 @@ module.exports = {
     presets: ["module:metro-react-native-babel-preset"],
     env: {
         production: {
-            plugins: ["react-native-paper/babel"],
+            plugins: [
+                "react-native-paper/babel",
+                [
+                    "module:react-native-dotenv",
+                    {
+                        moduleName: "@env",
+                        path: ".env",
+                    },
+                ],
+            ],
         },
     },
 }
