e2ff9fbb0cc8daadbb9f97e5aac63bcc632d361b
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-09 20:26:33 +0900

    ci: Node.jsのインストール手順を追加

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-echo "`ci_post_clone.sh` is running..."
+set -x
+
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+
+# Install Node.js
+NODE_VER=16
+VERSION=$(curl -s https://nodejs.org/dist/latest-v$NODE_VER.x/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')
+if [[ "$(arch)" == "arm64" ]]
+then
+  ARCH="arm64"
+else
+  ARCH="x64"
+fi
+
+curl "https://nodejs.org/dist/latest-v$NODE_VER.x/node-$VERSION-darwin-$ARCH.tar.gz" -o $HOME/Downloads/node.tar.gz
+tar -xf "$HOME/Downloads/node.tar.gz"
+NODE_PATH="$PWD/node-$VERSION-darwin-$ARCH/bin"
+PATH+=":$NODE_PATH"
+export PATH
+node -v
+npm -v
+
 
 # Install CocoaPods and yarn using Homebrew.
 brew install cocoapods
-echo "Installed Cocoapods"
-
 brew install yarn
-echo "Installed yarn"
 
 # Install dependencies
 yarn install
