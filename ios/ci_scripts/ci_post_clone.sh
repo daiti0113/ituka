7c5cecf17349d3a44c7ca4fc7f5a8634a67b9567
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-09 18:05:29 +0900

    ci: CocoaPodsインストール用のコマンドを修正

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+echo "`ci_post_clone.sh` is running..."
+
 # Install CocoaPods and yarn using Homebrew.
 brew install cocoapods
+echo "Installed Cocoapods"
+
 brew install yarn
+echo "Installed yarn"
 
 # Install dependencies
-yarn
+yarn install
 pod install
