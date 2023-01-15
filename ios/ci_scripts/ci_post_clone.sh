b0584f3677c86d4ac850974e0ed03720227cfb55
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-15 17:51:53 +0900

    ci: デバッグ用のログ出力を追加

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -14,9 +14,11 @@ yarn
 pod install
 
 echo ">>> Create GoogleService-Info.plist"
+echo ">>># 環境変数が読めるか確認"
+echo -n $GOOGLE_SERVICEINFO_PLIST_BASE64
 echo ">>># エンコードがうまくいっているか確認"
-echo -n $GOOGLE_SERVICEINFO_PLIST_BASE64 | base64 -D
-echo -n $GOOGLE_SERVICEINFO_PLIST_BASE64 | base64 -D > /Volumes/workspace/repository/ios/GoogleService-Info.plist
+echo -n $GOOGLE_SERVICEINFO_PLIST_BASE64 | base64 -d
+echo -n $GOOGLE_SERVICEINFO_PLIST_BASE64 | base64 -d > /Volumes/workspace/repository/ios/GoogleService-Info.plist
 
 # the sed command from RN cant find the file... so we have to run it ourselves
 sed -i -e $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
