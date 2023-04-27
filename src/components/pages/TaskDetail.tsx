1ef08f7f3a2760e4c4cf707850ac24129c8f8582
daiti0113 <stennis.ds.0113@gmail.com>
2023-04-27 22:02:31 +0900

    v1.0.0でリリースしないものをコメントアウト

--- src/components/pages/TaskDetail.tsx
+++ src/components/pages/TaskDetail.tsx
@@ -38,14 +38,14 @@ export const TaskDetail = () => {
                     </View>
                     <IconButton icon="dots-horizontal" iconColor={palette.neutral[900]} onPress={openMenu} />
                 </View>
-                <View style={styles.iconHolder}>
+                {/* <View style={styles.iconHolder}>
                     <IconLabel icon="heart" label="20" />
                     <IconLabel icon="bookmark" label="20" />
                     <IconLabel icon="comment" label="20" />
                     <View style={styles.iconHolderLeft}>
                         <IconLabel icon="eye" label="20" />
                     </View>
-                </View>
+                </View> */}
                 <View style={styles.body}>
                     <View style={styles.row}>
                         <Text variant="titleMedium" style={styles.rowTitle}>やることリスト</Text>
